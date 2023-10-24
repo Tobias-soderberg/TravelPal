d7e70cfa316cf323400d8a92879777a3c9a3b504
Tobias-soderberg <145038029+Tobias-soderberg@users.noreply.github.com>
2023-10-24 15:04:27 +0200

    Add travel halfway

--- TravelPal/AddTravelWindow.xaml.cs
+++ TravelPal/AddTravelWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 
 namespace TravelPal
@@ -8,6 +9,7 @@ namespace TravelPal
     /// </summary>
     public partial class AddTravelWindow : Window
     {
+        List<IPackingListItem> packingList = new();
         public AddTravelWindow()
         {
             InitializeComponent();
@@ -21,6 +23,8 @@ namespace TravelPal
 
             cbWorkVacation.Items.Add("Work");
             cbWorkVacation.Items.Add("Vacation");
+
+            txtAddTravelWarning.Visibility = Visibility.Hidden;
         }
 
         private void cboxTravelDocument_Checked(object sender, RoutedEventArgs e)
@@ -65,6 +69,51 @@ namespace TravelPal
             TravelsWindow travelsWindow = new TravelsWindow(UserManager.SignedInUser);
             travelsWindow.Show();
             Close();
+        }
+
+        private void btnAddTravel_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsValidInputs())
+            {
+                string destination = txtCity.Text;
+                int travellers = int.Parse(txtTravelers.Text);
+
+                if (cbWorkVacation.SelectedIndex == 0) //0 = Work
+                {
+                    Travel travel = new WorkTrip(destination, (Country)cbCountry.SelectedIndex, travellers, packingList, txtMeetingInfo.Text);
+                }
+                else if (cbWorkVacation.SelectedIndex == 1) //1 = Vacation
+                {
+                    Travel travel = new Vacation(destination, (Country)cbCountry.SelectedIndex, travellers, packingList, (bool)cboxAllInclusive.IsChecked!);
+                }
+
+                return;
+            }
+            txtAddTravelWarning.Visibility = Visibility.Visible;
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstPacking.SelectedIndex == -1)
+            {
+                return;
+            }
+            lstPacking.Items.Remove(lstPacking.SelectedItem);
+
+        }
+
+        private void btnAddItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        bool IsValidInputs()
+        {
+            string city = txtCity.Text.Trim();
+
+            string travellersString = txtTravelers.Text.Trim();
+
+            return (city.Length > 0 && int.TryParse(travellersString, out _) && cbCountry.SelectedIndex > 0 && cbWorkVacation.SelectedIndex != -1) ? true : false;
         }
     }
 }
